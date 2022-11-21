f0b7c3f5bc9007e89b1189898861c97fca6d800f
v4n99 <v4n99@mail.ru>
2022-11-22 01:01:12 +0300

    module init

--- src/main/java/org/is_matching_bipartite_max/MatchingBipartiteMaxModule.java
+++ src/main/java/org/is_matching_bipartite_max/MatchingBipartiteMaxModule.java
@@ -1,9 +1,24 @@
 package org.is_matching_bipartite_max;
 
 import com.mathsystem.api.graph.model.Graph;
+import com.mathsystem.api.graph.model.Vertex;
 import com.mathsystem.domain.plugin.plugintype.GraphProperty;
 
+import java.util.Map;
+import java.util.UUID;
+
 public class MatchingBipartiteMaxModule implements GraphProperty {
+    Graph graph;
+
+    public boolean dfs(int x){
+        boolean[] visited_vertices = new boolean[graph.getVertexCount()];
+        if(visited_vertices[x]){
+            return false;
+        }
+        visited_vertices[x] = true;
+
+        return false;
+    }
     @Override
     public boolean execute(Graph graph) {
 
