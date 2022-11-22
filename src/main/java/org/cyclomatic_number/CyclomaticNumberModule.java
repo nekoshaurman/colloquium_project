ec060af4efe9c4e29876814a7a55d02ac8d65e86
lebibop <nikolaevklim@gmail.com>
2022-11-22 15:45:36 +0300

    try to do CyclomaticNumberModule

--- src/main/java/org/cyclomatic_number/CyclomaticNumberModule.java
+++ src/main/java/org/cyclomatic_number/CyclomaticNumberModule.java
@@ -1,32 +1,31 @@
 package org.cyclomatic_number;
 
+import com.mathsystem.api.graph.model.Edge;
 import com.mathsystem.api.graph.model.Graph;
 import com.mathsystem.domain.plugin.plugintype.GraphCharacteristic;
 import java.util.*;
 
 public class CyclomaticNumberModule implements GraphCharacteristic
 {
-    ArrayList<int> g = new ArrayList<>(3);
 
-
-    void DFS(int v, boolean[] visited) {
+    void DFS(Integer v, boolean[] visited, List<Edge> edges) {
         visited[v] = true;
-        for (Integer adj : g.get(v))
+        for (Integer adj : edges.get(v))
         {
             if (!visited[adj])
             {
-                DFS(adj, visited);
+                DFS(adj, visited, edges);
             }
         }
     }
-    Integer Cyclomatic_Number(Integer vertexCount, Integer edgeCount) {
+    Integer Cyclomatic_Number(Integer vertexCount, Integer edgeCount, List<Edge> edges) {
         int count_connectivity_components = 0;
         boolean[] visited = new boolean[vertexCount];
         for (int i = 0; i < vertexCount; ++i)
         {
             if (!visited[i])
             {
-                DFS(i, visited);
+                DFS(i, visited, edges);
                 ++count_connectivity_components;
             }
         }
@@ -35,6 +34,7 @@ public class CyclomaticNumberModule implements GraphCharacteristic
 
     @Override
     public Integer execute(Graph graph) {
-        return Cyclomatic_Number(graph.getVertexCount(), graph.getEdgeCount());
+        List<Edge> edges = graph.getEdges();
+        return Cyclomatic_Number(graph.getVertexCount(), graph.getEdgeCount(), graph.getEdges());
     }
 }
