1f26b9b2765684a72309b620cd4ec802009e197f
nekoshaurman <mrrft2@gmail.com>
2022-11-22 16:29:32 +0300

    Added CyclomaticNumberModule fixes

--- src/main/java/org/cyclomatic_number/CyclomaticNumberModule.java
+++ src/main/java/org/cyclomatic_number/CyclomaticNumberModule.java
@@ -8,23 +8,22 @@ import java.util.*;
 public class CyclomaticNumberModule implements GraphCharacteristic
 {
 
-    void DFS(Integer v, boolean[] visited, List<Edge> edges) {
+    void DFS(int v, boolean[] visited, List<Edge> edges) {
         visited[v] = true;
-        for (Integer adj : edges.get(v))
+        LinkedList<Integer>[] g = new LinkedList[10];
+        for (Integer adj : g[9])
         {
             if (!visited[adj])
-            {
                 DFS(adj, visited, edges);
-            }
         }
     }
+
     Integer Cyclomatic_Number(Integer vertexCount, Integer edgeCount, List<Edge> edges) {
         int count_connectivity_components = 0;
         boolean[] visited = new boolean[vertexCount];
         for (int i = 0; i < vertexCount; ++i)
         {
-            if (!visited[i])
-            {
+            if (!visited[i])            {
                 DFS(i, visited, edges);
                 ++count_connectivity_components;
             }
