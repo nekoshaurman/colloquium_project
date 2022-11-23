5322c3f3757d521b80bd1d1ccf74d4fbecf466db
Govnocode4U <konstantinkozoriz1310@gmail.com>
2022-11-23 17:25:42 +0300

    MatchingBipartiteMaxModule.java fixed

--- src/main/java/org/is_matching_bipartite_max/MatchingBipartiteMaxModule.java
+++ src/main/java/org/is_matching_bipartite_max/MatchingBipartiteMaxModule.java
@@ -4,61 +4,65 @@ import com.mathsystem.api.graph.model.Edge;
 import com.mathsystem.api.graph.model.Graph;
 import com.mathsystem.api.graph.model.Vertex;
 import com.mathsystem.domain.plugin.plugintype.GraphProperty;
+
 import java.util.HashMap;
-import java.util.ArrayList;
 
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
+//TODO: Is a matching in a bipartite graph maximal?
 public class MatchingBipartiteMaxModule implements GraphProperty {
+    public boolean dfs(Graph graph, UUID vertex, HashMap<UUID, UUID> incedence_array, HashMap<UUID, Boolean> visited_vertices) {
+        UUID empty = new UUID(0, 0);
 
-//    public boolean dfs(int x){
-//        boolean[] visited_vertices = new boolean[graph.getVertexCount()];
-//        if(visited_vertices[x]){
-//            return false;
-//        }
-//        visited_vertices[x] = true;
-//        List<Edge> edge_list = graph.getEdges();
+        if (visited_vertices.get(vertex)) return false;
+        visited_vertices.replace(vertex, true);
 
-//        return false;
-//    }
-    @Override
-    public boolean execute(Graph graph) {
-        int x = graph.getVertexCount()-1;
-        boolean[] visited_vertices = new boolean[graph.getVertexCount()];
-        if(visited_vertices[x]){
-            return false;
-        }
-
-        visited_vertices[x] = true;
-        List<Edge> edge_list = graph.getEdges();
-        HashMap<UUID, List<UUID>> incidence_array = new HashMap<>();
-        for(int i = 0; i < graph.getEdgeCount(); i++){
-            ArrayList<UUID> values = new ArrayList<>();
-            UUID start_V = edge_list.get(i).getFromV();
-            incidence_array.put(start_V, values);
-        }
-
-        for(int i = 0; i < graph.getEdgeCount(); i++){
-            UUID start_V = edge_list.get(i).getFromV();
-            UUID end_V = edge_list.get(i).getToV();
-            ArrayList<UUID> values = (ArrayList<UUID>) incidence_array.get(start_V);
-            values.add(end_V);
-            incidence_array.put(start_V, values);
-        }
-
-        for(int i = 0; i < graph.getEdgeCount(); i++){
-            UUID temp = edge_list.get(i).getFromV();
-            System.out.println("\n" + temp + " вершина, массив - " + incidence_array.get(temp));
-        }
-
-        for(int i = 0; i < graph.getEdgeCount(); i++){
-            if(incidence_array.get(edge_list.get(i).getToV()) == null){
-                //incidence_array.get(edge_list.get())
+        for (UUID tmp_vert : incedence_array.keySet()) {
+            if (incedence_array.get(tmp_vert).equals(empty) || dfs(graph, tmp_vert, incedence_array, visited_vertices)) {
+                incedence_array.replace(tmp_vert, vertex);
+                return true;
             }
         }
-        return false;
 
+        return false;
     }
+
+
+    public void fill_arrays(HashMap<UUID, Boolean> visited_vertices) {
+
+        for (UUID vertex : visited_vertices.keySet()) {
+            visited_vertices.put(vertex, false);
+
+        }
+    }
+
+    @Override
+    public boolean execute(Graph graph) {
+        UUID empty = new UUID(0, 0);
+
+        HashMap<UUID, UUID> incedence_array = new HashMap<>(graph.getVertexCount());
+        System.out.println(incedence_array);
+        HashMap<UUID, Boolean> visited_vertices = new HashMap<>();
+        HashMap<UUID, Vertex> vertices = (HashMap<UUID, Vertex>) graph.getVertices();
+
+
+
+        for (UUID vertex : vertices.keySet()) {
+            incedence_array.put(vertex, empty);
+            visited_vertices.put(vertex, false);
+
+        }
+        for (UUID vertex : vertices.keySet()) {
+            fill_arrays(visited_vertices);
+            dfs(graph, vertex, incedence_array, visited_vertices);
+        }
+
+
+        System.out.println(incedence_array);
+        System.out.println(visited_vertices);
+
+        return false;
+    }
+
 }
