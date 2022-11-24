9b477e582fe4bf9bd11f689e799d13f35fe1104f
umlerr <ugrumov.misha@yandex.ru>
2022-11-24 15:01:35 +0300

    Reworked tests for CyclomaticNumberModule

--- src/test/java/org/cyclomatic_number/CyclomaticNumberModuleTest.java
+++ src/test/java/org/cyclomatic_number/CyclomaticNumberModuleTest.java
@@ -13,23 +13,25 @@ class CyclomaticNumberModuleTest {
     void TestCyclomaticNumberModule() throws FileNotFoundException {
         CyclomaticNumberModule cyclomaticNumberModule = new CyclomaticNumberModule();
 
-        File graph = new File("Test_cyclomatic_number/Test1_6.txt");
-        Assertions.assertEquals(6,
+        //UNDIRECTED TESTS
+
+        File graph = new File("Test_cyclomatic_number/Test1_3.txt");
+        Assertions.assertEquals(3,
                 cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
-        graph = new File("Test_cyclomatic_number/Test2_2.txt");
-        Assertions.assertEquals(2,
+        graph = new File("Test_cyclomatic_number/Test2_7.txt");
+        Assertions.assertEquals(7,
                 cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
-        graph = new File("Test_cyclomatic_number/Test3_4.txt");
-        Assertions.assertEquals(4,
+        graph = new File("Test_cyclomatic_number/Test3_0.txt");
+        Assertions.assertEquals(0,
+                cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
+        graph = new File("Test_cyclomatic_number/Test3_0.txt");
+        Assertions.assertEquals(0,
                 cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
         graph = new File("Test_cyclomatic_number/Test4_0.txt");
         Assertions.assertEquals(0,
                 cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
-        graph = new File("Test_cyclomatic_number/Test5_4.txt");
-        Assertions.assertEquals(5,
-                cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
-        graph = new File("Test_cyclomatic_number/Test6_8.txt");
-        Assertions.assertEquals(8,
+        graph = new File("Test_cyclomatic_number/Test5_13.txt");
+        Assertions.assertEquals(13,
                 cyclomaticNumberModule.execute(GraphFactory.loadGraphFromFile(graph)));
     }
 }
