9c0cb806c534cfbbd41eb5a898693489b2c81bf3
Anton <aksharov2003@gmail.com>
2022-12-24 16:00:12 +0300

    lenMinCycleDirectModule implemented

--- src/main/java/org/len_min_cycle_direct/LenMinCycleDirectModule.java
+++ src/main/java/org/len_min_cycle_direct/LenMinCycleDirectModule.java
@@ -1,12 +1,96 @@
 package org.len_min_cycle_direct;
 
+import com.mathsystem.api.graph.model.Edge;
 import com.mathsystem.api.graph.model.Graph;
+import com.mathsystem.api.graph.model.Vertex;
+import com.mathsystem.domain.graph.repository.GraphType;
 import com.mathsystem.domain.plugin.plugintype.GraphCharacteristic;
 
+import java.util.*;
+
 public class LenMinCycleDirectModule implements GraphCharacteristic {
+    Graph graph;
+    ArrayList<Vector<Integer>> adj;
+    int vertexCount;
+    int max_cycle_len;
+    UUID[] vertices;
+
     @Override
     public Integer execute(Graph graph) {
-        Integer len = 0;
-        return len;
+        this.graph = graph;
+
+        if (graph.getDirectType() != GraphType.DIRECTED) {
+            return -1;
+        }
+        initAdj();
+
+        return getMinCycleLen();
+    }
+
+    private int getMinCycleLen() {
+        this.max_cycle_len = this.vertexCount + 1;
+        int min_len = this.max_cycle_len;
+        for (int i = 0; i < vertexCount; i++) {
+            min_len = Math.min(getMinCycleLenFromVert(i), min_len);
+        }
+
+        if (min_len == this.max_cycle_len) {
+            min_len = 0;
+        }
+
+        return min_len;
+    }
+
+    private int getMinCycleLenFromVert(int start) {
+        int[] dist = new int[this.vertexCount];
+        Arrays.fill(dist, this.vertexCount + 1);
+        dist[start] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int v = q.poll();
+            for (int u : this.adj.get(v)) {
+                if (dist[u] > dist[v] + 1) {
+                    dist[u] = dist[v] + 1;
+                    q.add(u);
+                }
+                if (u == start && dist[start] == 0) {
+                    dist[start] = dist[v] + 1;
+                }
+            }
+        }
+
+        return dist[start];
+    }
+
+    private void initAdj() {
+        this.vertexCount = this.graph.getVertexCount();
+        this.adj = new ArrayList<>();
+        this.vertices = new UUID[vertexCount];
+
+        for (int i = 0; i < vertexCount; i++)
+            this.adj.add(new Vector<>());
+
+        // Добавляем все id вершин графа в массив вершин графа
+        int i = 0;
+        for (Map.Entry<UUID, Vertex> f : this.graph.getVertices().entrySet())
+            this.vertices[i++] = f.getKey();
+
+        // Проходимся по всем ребрам и заполняем список смежности(вершина откуда начинается ребро, куда идет ребро)
+        for (Edge edge : this.graph.getEdges()){
+            int from = findInVertices(edge.getFromV());
+            int to = findInVertices(edge.getToV());
+            this.adj.get(from).add(to);
+        }
+    }
+
+    private int findInVertices(UUID vertId) {
+        for (int i = 0; i < this.vertexCount; i++) {
+            if (this.vertices[i].equals(vertId))
+                return i;
+        }
+        return -1;
     }
 }
--- src/test/java/org/len_min_cycle_direct/LenMinCycleDirectModuleTest.java
+++ src/test/java/org/len_min_cycle_direct/LenMinCycleDirectModuleTest.java
@@ -13,8 +13,20 @@ class LenMinCycleDirectModuleTest {
     void TestLenMinCycleDirectModule() throws FileNotFoundException {
         LenMinCycleDirectModule lenMinCycleDirectModule = new LenMinCycleDirectModule();
 
-        File graph = new File("Test_len_min_direct/Test1_6.txt");
-        Assertions.assertEquals(6,
+        File graph = new File("Test_len_min_direct/test1_answer_3.txt");
+        Assertions.assertEquals(3,
+                lenMinCycleDirectModule.execute(GraphFactory.loadGraphFromFile(graph)));
+
+        graph = new File("Test_len_min_direct/test2_answer_4.txt");
+        Assertions.assertEquals(4,
+                lenMinCycleDirectModule.execute(GraphFactory.loadGraphFromFile(graph)));
+
+        graph = new File("Test_len_min_direct/test3_answer_7.txt");
+        Assertions.assertEquals(7,
+                lenMinCycleDirectModule.execute(GraphFactory.loadGraphFromFile(graph)));
+
+        graph = new File("Test_len_min_direct/test4_answer_0.txt");
+        Assertions.assertEquals(0,
                 lenMinCycleDirectModule.execute(GraphFactory.loadGraphFromFile(graph)));
     }
 }
