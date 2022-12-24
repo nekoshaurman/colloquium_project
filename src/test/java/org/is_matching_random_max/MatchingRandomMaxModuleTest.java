6869f28bb2681a0e1e61ecc93006382df147b06e
dvd <davidair17@gmail.com>
2022-12-24 22:27:13 +0300

    add amount of edges in max matching of graph

--- src/test/java/org/is_matching_random_max/MatchingRandomMaxModuleTest.java
+++ src/test/java/org/is_matching_random_max/MatchingRandomMaxModuleTest.java
@@ -16,5 +16,17 @@ class MatchingRandomMaxModuleTest {
         File graph = new File("Test_Random/Test1_3.txt");
         Assertions.assertEquals(3,
                 matchingRandomMaxModule.execute(GraphFactory.loadGraphFromFile(graph)));
+        graph = new File("Test_Random/Test2_4.txt");
+        Assertions.assertEquals(4,
+                matchingRandomMaxModule.execute(GraphFactory.loadGraphFromFile(graph)));
+        graph = new File("Test_Random/Test3_2.txt");
+        Assertions.assertEquals(2,
+                matchingRandomMaxModule.execute(GraphFactory.loadGraphFromFile(graph)));
+        graph = new File("Test_Random/Test4_3.txt");
+        Assertions.assertEquals(3,
+                matchingRandomMaxModule.execute(GraphFactory.loadGraphFromFile(graph)));
+        graph = new File("Test_Random/Test5_6.txt");
+        Assertions.assertEquals(6,
+                matchingRandomMaxModule.execute(GraphFactory.loadGraphFromFile(graph)));
     }
 }
